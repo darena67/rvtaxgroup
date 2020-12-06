489ccd2f1c52b6b0704f9cd6c0d4259600f9e360
jithesh-dev <jithesh.kumar.j@gmail.com>
2020-12-06 15:38:25 +0000

    Contact form fixed

--- src/components/contactForm/index.js
+++ src/components/contactForm/index.js
@@ -1,29 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactForm = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  };
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...formState }),
+    })
+      .then(() => alert("Success!"))
+      .catch((error) => alert(error));
+  };
+
   return (
     <form
       name="contact"
       className="contactForm"
       method="POST"
-      data-netlify-recaptcha="true"
       data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      onSubmit={handleSubmit}
     >
+      <input type="hidden" name="form-name" value="contact" />
       <label>
         Name
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          value={formState.name}
+        />
       </label>
       <label>
         Email
-        <input type="email" name="email" />
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={formState.email}
+        />
       </label>
       <label>
         Phone Number
-        <input type="text" name="phone" />
+        <input
+          type="text"
+          name="phone"
+          onChange={handleChange}
+          value={formState.phone}
+        />
       </label>
       <label>
         Message
-        <textarea name="message" rows="5"></textarea>
+        <textarea
+          name="message"
+          rows="5"
+          onChange={handleChange}
+          value={formState.message}
+        ></textarea>
       </label>
       <div data-netlify-recaptcha="true"></div>
       <button type="submit" value="Submit" className="btn">
