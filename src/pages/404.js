9a83207d986ffa54671a30a966882438ddf46abc
jithesh-dev <jithesh.kumar.j@gmail.com>
2020-12-06 16:38:13 +0000

    404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,33 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from "react";
+import Layout from "../components/layout";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
 const NotFoundPage = () => {
-  return (
-    <div className="notFound">
-      <Layout>
-        <h1>404 - Page Not Found</h1>
-      </Layout>
-    </div>
-  )
-}
+  const data = useStaticQuery(graphql`
+    query {
+      noFoundImage: file(relativePath: { eq: "404-Error.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 700) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
 
-export default NotFoundPage
+  return (
+    <Layout>
+      <div className="notFound container">
+        <Link className="notFound__link" to="/">
+          <Img
+            className="notFound__image"
+            fluid={data.noFoundImage.childImageSharp.fluid}
+          />
+        </Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
