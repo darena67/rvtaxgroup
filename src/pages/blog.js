b01157093f5501a20c5a1b6448a921f4afbf45ba
vladi <vladi@aresgate.net>
2020-12-22 21:17:48 -0800

    fixing seo..

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -9,7 +9,7 @@ import SEO from '../components/seo';
 const BlogPage = ({ data }) => {
   return (
     <div className='blog'>
-      <SEO pageTitle='Blog' />
+      <SEO pageTitle={`Blog | ${data.markdownRemark.frontmatter.title}`} />
       <Layout>
         <HeroOther
           title='blog'
